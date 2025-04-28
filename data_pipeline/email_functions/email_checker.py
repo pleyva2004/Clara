f41c3c6961b058e4382238f58e8b6a05f2888767
Pablo_Linux <pleyva2004@gmail.com>
2025-04-28 18:19:28 -0400

    Finished ceonncting data pipeline

--- data_pipeline/email_functions/email_checker.py
+++ data_pipeline/email_functions/email_checker.py
@@ -4,64 +4,18 @@ import re
 MAIL_PASSWORD = "gwjamkdovezjlfho" 
 MAIL_USERNAME = "njitshpe@gmail.com"
 
-def get_email_from():
+def getEmail():
     # Get date, subject and body len of all emails from INBOX folder
     with MailBox('imap.gmail.com').login(MAIL_USERNAME, MAIL_PASSWORD) as mailbox:
         for msg in mailbox.fetch(limit=1, reverse=True):
-            print("Email found and parsing...")
             body = ''
             
-            # if "Re: " in msg.subject:
-            #     print("Response Email...")
-            #     # print(msg.subject)
-
-
-            #     # Saving attachments into assets folder
-            #     # for att in msg.attachments:
-            #         # Print attachment info
-            #         # print(f"Attachment name: {att.filename}")
-            #         # print(f"Attachment type: {att.content_type}")
-                    
-            #         # # Save the attachment
-            #         # with open(f"downloads/{att.filename}", "wb") as f:
-            #         #     f.write(att.payload)
-            #         # print(f"Saved attachment: {att.filename}")
-                                
-            #     array = msg.text.split('\n')
-
-            #     # Remove all lines that start with '>' or '>>' and filter out njitshpe/njit.edu lines
-            #     array = [line for line in array if not (line.startswith('>') or 'njitshpe' in line or 'njit.edu' in line) and len(line) > 1]
-
-            #     length = len(array)
-            #     i = 0 
-            #     while i < length:
-
-            #         # Iterate through the array in reverse order
-            #         line = array[length - i - 1]
-
-            #         # Check for signature pattern (1-2 words followed by comma and carriage return)
-            #         if re.search(r'^\w+(?:\s+\w+)?,\r$', line) or re.search(r'^-- \r', line):
-            #             print('Signature found')
-            #             break
-
-            #         i += 1
-
-            #     for j in range(i):
-            #         # Iterate through the array in forward order
-            #         line = array[j]
-
-            #         # Stop if we find "My Best,"
-            #         if "My Best," in line:
-            #             print('Signature found')
-            #             break
-
-            #         print(line[:-1])
-            #         body += line[:-1] + " "
-
-
-            print("SHPE Email...")
+            if "Re: " in msg.subject:
+                print("Response Email...")
                 # print(msg.subject)
-
+            else:
+                print("SHPE Email...")
+        
 
                 # Saving attachments into assets folder
                 # for att in msg.attachments:
@@ -98,7 +52,6 @@ def get_email_from():
                     break
                 i += 1
 
-
             # After signature is found, iterate through the array in forward order
 
             # Line index for Iterate through the array in forward order
@@ -108,4 +61,3 @@ def get_email_from():
                 body += line[:-1] + " "
 
             return body, msg.from_, msg.subject    
-print(get_email_from())
---
+++ data_pipeline/email_functions/email_sender.py
@@ -0,0 +1,11 @@
+'''
+This file is used to send the email to the specific sender
+
+'''
+
+def sendEmail(sender, subject, body):
+    pass
+
+
+
+

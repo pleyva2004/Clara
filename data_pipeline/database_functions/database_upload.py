16d1acc80a656dc5a2be72099c570f7c64460156
RiftIcy <ultron895@gmail.com>
2025-05-01 17:37:57 -0400

    dyanmic tables

--- data_pipeline/database_functions/database_upload.py
+++ data_pipeline/database_functions/database_upload.py
@@ -1,6 +1,6 @@
 import json
+import re
 from connect import get_connection
-
 
 # Takes the already parsed data and puts it into the database
 '''
@@ -21,16 +21,36 @@ def extract(conn, file_path):
         body = data.get("Body", {})
         sender = body.get("Sender")
         subject = body.get("Subject")
-        email_contents = body.get("Email_Contents")
-        attachments = body.get("Attachments")
+        email_contents = body.get("Email_Contents", "No Conntent")
+        attachments = body.get("Attachments", "No Attachments")
+
+
+        # Grab company from Json
+        raw_partner = body.get("Partner")
+        
+        # Remove special characters from the company name
+        table_name = re.sub(r'\W+', '_', raw_partner).lower()
         cursor = conn.cursor()
-        insert_query = """
-        INSERT INTO Email_Inbox (sender, subject, email_content, attachments)
+
+        # If the table does not exist, create it
+        create_table_query = f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            sender TEXT,
+            subject TEXT,
+            email_content TEXT,
+            attachments TEXT
+        )
+        """
+        cursor.execute(create_table_query)
+        
+        #insert the data into the table
+        insert_query = f"""
+        INSERT INTO {table_name} (sender, subject, email_content, attachments)
         VALUES (%s, %s, %s, %s)
         """
 
         cursor.execute(insert_query, (sender, subject, email_contents, attachments))
-        conn.commit() # Save the changes
+        conn.commit() # Save the changes    
         print("Data transfer Successful")
     except Exception as e:
         print(f"Error: {e}")
@@ -40,6 +60,6 @@ def extract(conn, file_path):
             print("Database connection closed")
 
 
-json_file_path = "/home/lior/GitHub/Clara/data_pipeline/Tests/temp.json" # Must give a path the the 
+json_file_path = "/home/lior/GitRepo/Clara/data_pipeline/Tests/test1.json" # Must give a path the the 
 conn = get_connection("sql1.njit.edu", "lb356", "123Luigi895@", "lb356") 
 extract(conn, json_file_path)

963d4393a749c6bab8b799f0d9cf28dac7c987b6
Lior <ultron895@gmail.com>
2025-05-25 16:15:22 -0400

    Cleaning up codespace

--- telegram_bot/find_chat_id.py
+++
@@ -1,7 +0,0 @@
-from telethon.tl.types import UpdateChatParticipants
-
-def get_chat_id(result):
-    for upd in result.updates.updates:
-            if isinstance(upd, UpdateChatParticipants):
-                chat_id = upd.participants.chat_id
-                print("Chat ID (from participants):", chat_id)
